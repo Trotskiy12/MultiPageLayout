78c143d949c3c6f3b2d563585486550a284603bb
Trotskiy12 <mebelend33@gmail.com>
2021-11-04 13:32:36 +0300

    Adaptive final

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,24 @@
 // import mixitup from 'mixitup';
 $(function () {
 
-    $('.header__btn').on('click', function(){
+    $('.header__btn').on('click', function () {
         $('.rightside-menu').removeClass('rightside-menu--close');
     });
-    $('.rightside-menu__close').on('click', function(){
+    $('.rightside-menu__close').on('click', function () {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
 
-    $('.header__btn-mobile').on('click', function(){
+    $('.header__btn-mobile').on('click', function () {
         $('.menu').toggleClass('menu--open');
     });
 
 
-    if($(window).width() < 651){
+    if ($(window).width() < 651) {
         $('.works-path__item--meas').appendTo($('.works-path__items-box'));
     }
 
 
-    $('.top__slider').slick({ 
+    $('.top__slider').slick({
         // точки вслючить
         dots: true,
         // стрелки выключить
@@ -29,16 +29,60 @@ $(function () {
         // autoplay: true
     });
 
-    $('.contact-slider').slick({ 
+    $('.contact-slider').slick({
         slidesToShow: 10,
         slidesToScroll: 10,
         // точки вслючить
         dots: true,
         // стрелки выключить
         arrows: false,
+        responsive: [
+            {
+                breakpoint: 1700,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                }
+            },
+            {
+                breakpoint: 1511,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 551,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 376,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+        ]
     });
 
-    $('.article-slider__box').slick({ 
+    $('.article-slider__box').slick({
         prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-left.svg" alt="arrow left"></button>',
         nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowrigth"><img src="images/arrow-rigth.svg" alt="arrow rigth"></button>'
     });
